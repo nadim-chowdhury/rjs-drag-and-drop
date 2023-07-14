1d82682a124173f533ba192a0082a7bdffd8c449
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-15 01:28:02 +0600

    code optimized

--- src/test.jsx
+++ src/test.jsx
@@ -1,12 +1,12 @@
-{
-  users.map((user) => {
-    return (
-      <tr key={user.id}>
-        <th>{user.id}</th>
-        <td>{user.name}</td>
-        <td>{user.username}</td>
-        <td>{user.email}</td>
-      </tr>
-    );
-  });
-}
+// {
+//   users.map((user) => {
+//     return (
+//       <tr key={user.id}>
+//         <th>{user.id}</th>
+//         <td>{user.name}</td>
+//         <td>{user.username}</td>
+//         <td>{user.email}</td>
+//       </tr>
+//     );
+//   });
+// }
