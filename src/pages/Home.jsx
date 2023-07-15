0b808a582d43157f4dd9a3c89e1107015f100e47
Nadim <Nadim@chowdhury.com>
2023-07-15 12:33:17 +0600

    data fetched

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>This is Home</h1>
